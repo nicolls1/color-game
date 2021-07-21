68b90b8fc1b30a956b168b994f4f6cfda76f8268
Matt Nicolls <2540582+nicolls1@users.noreply.github.com>
2021-07-22 00:41:20 +0200

    add script to set questions in firebase

---
+++ system/populateQuestions.js
@@ -0,0 +1,40 @@
+/**
+ * Set all questions.
+ *
+ * Run from project root with: node system/populateQuestions.js
+ */
+const admin = require('firebase-admin')
+
+var questions = require('./../data/questions.json')
+
+let firebase
+const LOCAL_EMULATOR = true
+if (LOCAL_EMULATOR) {
+  firebase = admin.initializeApp()
+  firebase.firestore().settings({
+    host: 'localhost:8081',
+    ssl: false,
+  })
+} else {
+  var stageServiceAccount = require('./../color-game-key.json')
+  firebase = admin.initializeApp({
+    credential: admin.credential.cert(stageServiceAccount),
+  })
+}
+
+const firestore = admin.firestore(firebase)
+
+const setQuestions = async () => {
+  const writer = firestore.bulkWriter()
+  questions.forEach((question, idx) => {
+    writer.create(
+      firestore.collection('questions').doc(idx.toString()),
+      question
+    )
+  })
+  writer.create(firestore.collection('questions').doc('meta'), {
+    length: questions.length,
+  })
+  await writer.close()
+}
+setQuestions()
